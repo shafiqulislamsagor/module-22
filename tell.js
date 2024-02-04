4430840d4ff806e0d4df0f075a0a051d1094e2d9
shafiqulislamsagor <shafiqulislamsagor277@gmail.com>
2024-02-04 23:54:34 +0600

    compair

---
+++ tell.js
@@ -0,0 +1,15 @@
+// const heights = [65,100, 66,68,72,77,60,58,79,96,52];
+
+// function getMax(numbers){
+//     let max = numbers[0];
+//     for(const num of numbers){
+//         if(num > max){
+//             max = num;
+//         }
+//     }
+//     return max;
+// }
+
+// const max = getMax(heights);
+
+// console.log('max value is -',max)
