ebdb6c2366b048b34cbe75dcfa9009aa5f243672
shafiqulislamsagor <shafiqulislamsagor277@gmail.com>
2024-02-04 23:59:15 +0600

    smallest

--- tell.js
+++ tell.js
@@ -13,3 +13,19 @@
 // const max = getMax(heights);
 
 // console.log('max value is -',max)
+
+const heights = [65,100,22, 66,68,72,77,60,58,79,96,52];
+
+function small(inc){
+    let small = inc[0];
+    for(let i of inc){
+        if(small > i){
+            small = i;
+        }
+    }
+    return small;
+}
+
+const smallPerson = small(heights);
+
+console.log(smallPerson);
