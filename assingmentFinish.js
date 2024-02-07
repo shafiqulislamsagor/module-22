1004ee5a09a7b100eea09afca8007bef0775eb5c
shafiqulislamsagor <shafiqulislamsagor277@gmail.com>
2024-02-07 22:24:40 +0600

    A-4-2

--- assingmentFinish.js
+++ assingmentFinish.js
@@ -12,3 +12,39 @@ function calculateMoney(ticketSale) {
     }
     return 'Invalid Number...!'
 }
+
+
+function checkName(name) {
+    if (typeof name !== 'string') {
+        return 'Invalid..!Please..String data types..'
+    }
+    let arry = name.toLowerCase().split('');
+    let check = arry.length - 1;
+    if(arry[check] == 'a' ){
+        return 'Good name'
+    }
+    else if(arry[check] == 'a'){
+        return 'Good name'
+    }
+    else if(arry[check] == 'y'){
+        return 'Good name'
+    }
+    else if(arry[check] == 'i'){
+        return 'Good name'
+    }
+    else if(arry[check] == 'e'){
+        return 'Good name'
+    }
+    else if(arry[check] == 'o'){
+        return 'Good name'
+    }
+    else if(arry[check] == 'u'){
+        return 'Good name'
+    }
+    else if(arry[check] == 'w'){
+        return 'Good name'
+    }
+    else{
+        return 'Bad Name'
+    }
+}
