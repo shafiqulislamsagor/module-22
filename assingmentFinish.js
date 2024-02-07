c81ce2f84ccf50fb4087fe1a718a398dfc363a09
shafiqulislamsagor <shafiqulislamsagor277@gmail.com>
2024-02-08 00:13:17 +0600

    A-4-4

--- assingmentFinish.js
+++ assingmentFinish.js
@@ -63,3 +63,24 @@ function deleteInvalids(array) {
     }
     return newArray;
 }
+
+
+
+
+function password(obj) {
+    if(!obj.name || !obj.birthYear|| obj.birthYear < 1000 || obj.birthYear > 9999 || !obj.siteName){
+        return 'Invalid... Input correct now'
+    }
+    let upper = objt.siteName.split('');
+    let upperWord = '';
+    for(let i = 0 ; i < upper.length ; i++){
+        if(i == 0){
+            upperWord += upper[i].toUpperCase()
+        }
+        else{
+            upperWord += upper[i]
+        }
+    }
+    let result = upperWord.concat('#').concat(obj.name).concat('@').concat(obj.birthYear)
+    return result;
+}
---
+++ testing.js
@@ -0,0 +1,12 @@
+const objt = { name: "sagor" , birthYear: 199 , siteName: "google" }
+
+if(objt.birthYear > 999 && objt.birthYear < 10000){
+    console.log('thik ase')
+}
+else{
+    console.log('thik nai')
+}
+
+
+
+
